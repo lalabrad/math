847600fd2b5213554e09d9552fdd064f81fc5298
yao <ywang305@syr.edu>
2021-05-02 17:16:56 -0400

    Initial Commit

---
+++ math/mathApp.swift
@@ -0,0 +1,17 @@
+//
+//  mathApp.swift
+//  math
+//
+//  Created by Yao Wang on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct mathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
